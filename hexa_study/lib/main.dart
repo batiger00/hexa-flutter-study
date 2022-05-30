2322c310d359e47258b7f20c367839772a5e2d5b
batiger00 <batiger00@gmail.com>
2022-05-31 00:36:16 +0900

    2022.05.31 study

--- hexa_study/lib/main.dart
+++ hexa_study/lib/main.dart
@@ -1,75 +1,52 @@
-// ignore_for_file: prefer_const_constructors, avoid_print
+// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Appbar',
-      theme: ThemeData(
-      primarySwatch: Colors.red
-      ),
-    home: MyPage(),
+      title: 'Sanck Bar',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: MyPage(),
     );
   }
 }
 
 class MyPage extends StatelessWidget {
-  const MyPage({ Key? key }) : super(key: key);
+  const MyPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Appbar icon menu'),
-        centerTitle: true,
-        elevation: 0.0,
-        actions: <Widget>[
-          IconButton(
-          icon: Icon(Icons.shopping_cart),
-          onPressed: () {
-            print('Shopping cart button is clicked');
-          },
+        appBar: AppBar(
+          title: Text('Snack Bar'),
+          centerTitle: true,
         ),
-        IconButton(
-          icon: Icon(Icons.search),
-          onPressed: () {
-            print('Search button is clicked');
-          },
-        ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/fire.jpg'),
-                backgroundColor: Colors.white,
+        body: Builder(
+          builder: (BuildContext ctx) {
+            return Center(
+              child: ElevatedButton(
+                child: Text(
+                  'Show me',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                ),
+                onPressed: () {
+                  ScaffoldMessenger.of(ctx)
+                      .showSnackBar(SnackBar(content: Text("Hellow")));
+                },
               ),
-              accountName: Text('Fire'),
-              accountEmail: Text('batiger00@gmail.com'),
-              onDetailsPressed:(){
-                print('arrow is clicked');
-              },
-              decoration: BoxDecoration(
-                color: Colors.red[200],
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(40),
-                  bottomRight: Radius.circular(40),
-                )
-              ),
-            )
-          ],
-           
-        ),
-      ),
-    );
+            );
+          },
+        ));
   }
 }
